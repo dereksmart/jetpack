5bf57987153597b7816dbb6332815c4129005d85
dereksmart <smart@automattic.com>
2016-05-26 14:52:53 -0400

    kick off the tests

--- tests/php/test_class.jetpack.php
+++ tests/php/test_class.jetpack.php
@@ -176,7 +176,7 @@ EXPECTED;
 		Jetpack::init()->implode_frontend_css( true );
 
 		$seen_bundle = false;
-		foreach ( $wp_styles->registered as $handle => $handle_obj ) {
+		foreach ( $wp_styles->registered as $handle => $handle_obj ) { 
 			if ( $style_handle === $handle ) {
 				$expected = ( defined( 'WP_DEBUG' ) && WP_DEBUG ) ? "<!-- `{$style_handle}` is included in the concatenated jetpack.css -->\r\n" : '';
 				$this->assertEquals( $expected, get_echo( array( $wp_styles, 'do_item' ), array( $handle ) ) );
